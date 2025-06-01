4bd0f1c28b436f975a33dd1f929f8e209cbb9b7c
Dominic Bergmann <dominic.bergmann@gmail.com>
2025-06-01 16:07:18 +0200

    Update create_db to include initial DB creation with autoerase of old DB

--- create_db.py
+++ create_db.py
@@ -1,7 +1,23 @@
 import sqlite3
 import os
 
-def create_db():
-    print(os.path.exists("imaginary_db.db"))
+def create_database():
+    if os.path.exists("ships.db"):
+        os.remove("ships.db")
 
-create_db()
+    conn = sqlite3.connect("ships.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    CREATE TABLE ships (
+        ship TEXT PRIMARY KEY,
+        weapon TEXT,
+        hull TEXT,
+        engine TEXT
+    )""")
+
+    conn.commit()
+    conn.close()
+    print("DB created")
+
+create_database()
